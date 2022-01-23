1b032c2ab8f397c0533d8fc24b6f380baaa74801
zinmyomaung <zinmyomaung.zotefamily@gmail.com>
2022-01-23 15:05:32 +0630

    Episode 13 - Fetching API in React

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Joke from './Joke';
+import Reddit from './Reddit';
+
+function App() {
+  const [redditVisible, setRedditVisible] = useState(false);
+  const [jokeVisible, setJokeVisible] = useState(false);
+
+  return (
+    <div>
+      <div className="buttons">
+        <button
+          onClick={() => setRedditVisible(prevRedditVisible => !redditVisible)}
+        >Toggle Reddit</button>
+        <button
+          onClick={() => setJokeVisible(prevJokeVisible => !jokeVisible)}
+        >Toggle Joke</button>
+      </div>
+      {redditVisible && <Reddit/>}
+      {jokeVisible && <Joke/>}
+    </div>
+  );
+}
+
+export default App;
